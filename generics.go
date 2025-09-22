ffee9b37d41d57e53ba6dc1179c5fccd59f9b61f
kalacz123 <kalaczynskibo@gmail.com>
2025-09-22 09:07:32 +0300

    finished generics

---
+++ generics.go
@@ -0,0 +1,77 @@
+package main
+
+import "fmt"
+
+// func swap[T any](a, b T) (T, T) {
+// 	return b, a
+// }
+
+type Stack[T any] struct {
+	elements []T
+}
+
+func (s *Stack[T]) push(element T){
+	s.elements = append(s.elements, element)
+}
+
+func (s *Stack[T]) pop() (T, bool){
+	if len(s.elements) == 0 {
+		var zero T
+		return zero, false
+	}
+	element := s.elements[len(s.elements)-1]
+	s.elements = s.elements[:len(s.elements)-1]
+	return element, true
+}
+
+func (s *Stack[T]) isEmpty() bool{
+	return len(s.elements) == 0
+}
+
+func (s Stack[T]) printAll() {
+	if len(s.elements) == 0 {
+		fmt.Println("The stack is empty")
+		return
+	}
+	for _, element := range s.elements {
+		fmt.Println(element)
+	}
+}
+
+func main() {
+
+	// a, b := swap(1, 2)
+	// fmt.Println(a, b)
+
+	// name1, name2 := "John", "Jane"
+	// name1, name2 = swap(name1, name2)
+	// fmt.Println(name1, name2)
+
+	intStack := Stack[int]{}
+	intStack.push(1)
+	intStack.push(2)
+	intStack.push(3)
+	intStack.printAll()
+	intStack.pop()
+	intStack.printAll()
+	if intStack.isEmpty() {
+		fmt.Println("The stack is empty")
+	} else {
+		fmt.Println("The stack is not empty")
+	}
+	stringStack := Stack[string]{}
+	stringStack.push("Hello")
+	stringStack.push("World")
+	stringStack.push("!")
+	stringStack.printAll()
+	stringStack.pop()
+	stringStack.printAll()
+	if stringStack.isEmpty() {
+		fmt.Println("The stack is empty")
+	} else {
+		fmt.Println("The stack is not empty")
+	}
+
+
+
+}
